64b418dbd3d6983691e30c97d6430fec67301468
Leonardo Torres <2212.leonardo@gmail.com>
2025-12-28 06:04:03 -0300

    fixing prisma

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations..."
+echo "Generating Prisma client..."
+npx prisma generate
 
-npx prisma db push
+echo "Running database migrations..."
+npx prisma migrate deploy
 
 echo "Starting application..."
 exec node server.js
